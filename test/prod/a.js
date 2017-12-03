8db4a5dfe21c0f5fb1218b64b9f941e5e2b9005b
Casey Foster <c@sey.me>
2017-12-03 14:46:08 -0600

    Fix tests

--- test/prod/a.js
+++ test/prod/a.js
@@ -1,4 +1,4 @@
 const style = document.createElement("style");
-style.innerHTML = "a{color:red}\n";
+style.innerHTML = "a{color:red}";
 document.head.appendChild(style);
 export default {};
--- test/prod/c/c.js
+++ test/prod/c/c.js
@@ -1,4 +1,4 @@
 const style = document.createElement("style");
-style.innerHTML = "#iYTGA{color:#00f}.iYTGA .qHPlA:hover,.tsmQC #q7cQH{color:purple}.iYTGA .foo:hover{color:orange}\n";
+style.innerHTML = "#iYTGA{color:#00f}.iYTGA .qHPlA:hover,.tsmQC #q7cQH{color:purple}.iYTGA .foo:hover{color:orange}";
 document.head.appendChild(style);
 export default {"bar":"tsmQC","foo":"iYTGA"};
